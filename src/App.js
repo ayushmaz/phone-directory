6ce2d93e389d6589c233e3f9d16de112cabe34ad
Ayush Majumder <majumdershuvu@gmail.com>
2020-05-17 21:24:19 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import React from 'react';
 function App() {
   return (
     <div >
-      Phone Directory
+      {/* <div>Phone Directory</div>
+      <span>Name</span> <br/>
+      <span>Ayush</span>
+      <br/>
+      <button>Add file</button> */}
+      <label htmlFor="name">Name: </label>
+      <input type="text" id = "name" defaultValue="Ayush"></input>
     </div>
   );
 }
