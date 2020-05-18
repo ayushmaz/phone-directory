628ecd61d4d2b04c57a70ab9f64f8bb46ad1eb43
Ayush Majumder <majumdershuvu@gmail.com>
2020-05-18 21:14:00 +0530

    created 'Header' component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import Header from './Header'
 
 function App() {
   return (
     <div >
-      {/* <div>Phone Directory</div>
+      <Header/>
       <span>Name</span> <br/>
       <span>Ayush</span>
       <br/>
-      <button>Add file</button> */}
-      <label htmlFor="name">Name: </label>
-      <input type="text" id = "name" defaultValue="Ayush"></input>
+      <button>Add file</button>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = function(){
+    return(
+        <div>Phone Directory</div>
+    );
+}
+
+
+//we dont need a class component as there is no state
+// class Header extends Component{
+//     render(){
+//         return(<div>Phone Directory</div>);
+//     }
+// }
+
+export default Header;
