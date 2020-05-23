2756542afe73483b4255151006c5ac73eba83424
Ayush Majumder <majumdershuvu@gmail.com>
2020-05-24 00:29:27 +0530

    Updates

---
+++ precache-manifest.9b5dc0862d0885290db6b0f16a10056e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce81876c1ce8764282a20045ac1de61",
+    "url": "/phone-directory/index.html"
+  },
+  {
+    "revision": "3f378a731bce10f333f5",
+    "url": "/phone-directory/static/css/main.31e649ec.chunk.css"
+  },
+  {
+    "revision": "60ac76e056b912604669",
+    "url": "/phone-directory/static/js/2.79ea12bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/phone-directory/static/js/2.79ea12bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f378a731bce10f333f5",
+    "url": "/phone-directory/static/js/main.2303b162.chunk.js"
+  },
+  {
+    "revision": "5e439a64b47b58e7ba8b",
+    "url": "/phone-directory/static/js/runtime-main.0fc077e2.js"
+  }
+]);
