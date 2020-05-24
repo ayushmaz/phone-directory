fa35070c195d58c8419966f1c5aea44014e9d7c7
Ayush Majumder <majumdershuvu@gmail.com>
2020-05-24 20:24:57 +0530

    Updates

--- precache-manifest.44c5e7b965bd930e6bc60c6c3085d6db.js
+++ precache-manifest.44c5e7b965bd930e6bc60c6c3085d6db.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "096bf1e4e247aa8ad92d42d2ee5a8f47",
+    "revision": "c4d0d411fd54bbc7a14eb6bb0578d302",
     "url": "/phone-directory/index.html"
   },
   {
-    "revision": "a2d63c7ac6f5e8be6f0b",
+    "revision": "705a7cdc8dafb438ef2e",
     "url": "/phone-directory/static/css/main.31e649ec.chunk.css"
   },
   {
-    "revision": "e0d9a15850b32f65f940",
-    "url": "/phone-directory/static/js/2.6b1867dd.chunk.js"
+    "revision": "60ac76e056b912604669",
+    "url": "/phone-directory/static/js/2.79ea12bf.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/phone-directory/static/js/2.6b1867dd.chunk.js.LICENSE.txt"
+    "url": "/phone-directory/static/js/2.79ea12bf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a2d63c7ac6f5e8be6f0b",
-    "url": "/phone-directory/static/js/main.57e51ae2.chunk.js"
+    "revision": "705a7cdc8dafb438ef2e",
+    "url": "/phone-directory/static/js/main.a7ac2a22.chunk.js"
   },
   {
     "revision": "5e439a64b47b58e7ba8b",
